7f3f7ef659548eae4fe1902c90b46c03728d1d1a
Sbuda32 <sbuda32@gmail.com>
2019-03-12 12:39:40 +0200

    added view folder, index.pug file and modified app.js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 //Add modules
-const app = require('express');
+const app = require('express')();
 const http = require('http');
 
 //create http server
-http.createServer(app);
+const httpServer = http.createServer(app);
+
+//Set template engine and views folder
+app.use('views engine', 'pug');
+app.use('views', '/views/');
 
 app.get('/', (req, res) => {
     res.send('<h1> Hello World </h1>');
 });
 
-http.listen(8080);
+httpServer.listen(8080, () => {
+    console.log('Http server running');
+});
