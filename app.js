e8219b946ca4eba7546c7b42f0422a3d8d8361eb
Sbuda32 <sbuda32@gmail.com>
2019-03-12 18:45:40 +0200

    Added the public, socket.io folders, and modified app.js and index.pug

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 //Add modules
-const app = require('express')();
+const express = require('express');
 const http = require('http');
+const socket = require('socket.io');
+
+//Initialize express app
+const app = express();
 
 //create http server
 const httpServer = http.createServer(app);
+
+//Bring public folder on load
+app.use(express.static('/public'));
+
+//Initialize Socket.io
+const io = socket(httpServer);
 
 //Set template engine and views folder
 app.set('view engine', 'pug');
@@ -11,6 +21,10 @@ app.set('views', './views/');
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected');
 });
 
 httpServer.listen(7000, () => {
