9c81f20a68388e8fc0824b3e06de7696c30048ff
Sbuda32 <sbuda32@gmail.com>
2019-03-19 16:25:56 +0200

    Modified app.js, welcome.pug, index.pug and added a file chat-box.pug

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const path = require('path');
 //Initialize express app
 const app = express();
 
+//Declaring array to hold nicknames
+var nicknames = [];
+
 //create http server
 const httpServer = http.createServer(app);
 
@@ -24,10 +27,26 @@ app.get('/', (req, res) => {
     res.render('welcome');
 });
 
+app.get('/chat-box', (req, res) => {
+    res.render('chat-box', {basedir: __dirname + '/views'});
+});
+
 io.on('connection', (socket) => {
     console.log('user connected');
     socket.on('connect', () => {
         console.log('User connected');
+    });
+
+    socket.on('new user', (data, callback) => {
+        console.log('new user entered');
+        if(nicknames.indexOf(data) != -1){
+            callback(false);
+        }
+        else{
+            callback(true);
+            socket.nickname = data;
+            nicknames.push(socket.nickname);
+        }
     });
 
     socket.on('chat message', (msg) => {
